dee071cdae109f055febe1344f44bd4d14875b39
Haerim <34bestsun@gmail.com>
2021-02-26 00:14:27 +0900

    20200225_06_mission_alert

--- 05mission_Alert/ViewController.swift
+++ 05mission_Alert/ViewController.swift
@@ -8,12 +8,54 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let timeSelector: Selector = #selector(ViewController.updateTime)
+    let interval = 1.0
+    var count = 0
+    var alarmTime: String?
+    var alertFlag = false //얼럿 확인 버튼이 눌렸는지 저장할 변수
+    
+    @IBOutlet var lblCurrentTime: UILabel!
+    @IBOutlet var lblPickerTime: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        Timer.scheduledTimer(timeInterval: interval, target: self, selector: timeSelector, userInfo: nil, repeats: true)
     }
-
+    
+    @IBAction func changeDatePicker(_ sender: UIDatePicker) {
+        let datePickerView = sender
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm EEE"
+        lblPickerTime.text = formatter.string(from: datePickerView.date)
+        formatter.dateFormat = "hh:mm aaa"
+        alarmTime = formatter.string(from: datePickerView.date)
+    }
+    
+    @objc func updateTime(){
+        let date = NSDate()
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss EEE"
+        lblCurrentTime.text = formatter.string(from: date as Date)
+        
+        formatter.dateFormat = "hh:mm aaa"
+        let currentTime = formatter.string(from: date as Date)
+        if (alarmTime == currentTime){ //알람시간 = 현재시간이 같으면
+            if !alertFlag { //alertFlag = false이면
+                let lampOnAlert = UIAlertController(title: "알림", message: "설정된 시간입니다!!", preferredStyle: UIAlertController.Style.alert)
+                let onAction = UIAlertAction(title: "네 알겠습니다", style: UIAlertAction.Style.default, handler: nil)
+                lampOnAlert.addAction(onAction)
+                present(lampOnAlert, animated: true, completion: nil)
+                alertFlag = true
+            }
+        }
+        else{
+            alertFlag = false
+        }
+    }
+    
 
 }
 
